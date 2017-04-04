12f425e5c453fc300e29aadbdd60ba1d884b2c3b
stevenwjps <steven.lee@wjps.co.uk>
2017-04-04 16:46:33 +0100

    auto loop

--- js/panel.js
+++ js/panel.js
@@ -1,9 +1,19 @@
+var i = 0;
+var j = 0;
 PanelLoop();
 
 function PanelLoop(){
   setTimeout(function(){    
     PanelStatus();
-    PanelLoop();    
+    PanelLoop();  
+    i++;
+    if(i == 300){
+      i = 0;
+      j++;
+    }  
+    if(j == 4){
+      j = 0;
+    }
   },1000);
 }
 
@@ -11,6 +21,9 @@ function PanelStatus(){
   $.getJSON("/Data/panelStatus.php", function(status){
   		
       switch (status.Status) {
+        case "0":
+          changeLoop();
+          break;
         case "1":        
             $("#toggleablearea_tasks").show();
             $("#toggleablearea_toggl").hide();
@@ -56,4 +69,31 @@ function PanelStatus(){
       }
       
   	});
+}
+
+function changeLoop(){
+    switch (j) {
+      case 1:          
+          $("#toggleablearea_tasks").show();
+          $("#toggleablearea_toggl").hide();
+          $("#toggleablearea_alfie").hide();
+          $("#toggleablearea_WJPS").hide();
+          $("#toggleablearea_WCSStatus").hide();
+        break;
+      case 2:
+          $("#toggleablearea_tasks").hide();
+          $("#toggleablearea_toggl").show();
+          $("#toggleablearea_alfie").hide();
+          $("#toggleablearea_WJPS").hide();
+          $("#toggleablearea_WCSStatus").hide();
+        break;
+      case 3:      
+          $("#toggleablearea_tasks").hide();
+          $("#toggleablearea_toggl").hide();
+          $("#toggleablearea_alfie").hide();
+          $("#toggleablearea_WJPS").hide();
+          $("#toggleablearea_WCSStatus").show();
+        break;     
+    }        
+    
 }
